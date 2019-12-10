710ed670b82d788270a676302ecc7f2b02ac3630
chuongdv <chuongdv98@gmail.com>
2019-12-11 00:55:28 +0700

    add modul AI and update GUI

--- GUI.py
+++ GUI.py
@@ -4,13 +4,13 @@ from tkinter import Canvas
 from tkinter import Button
 import tkinter
 from PIL import Image, ImageTk
+from tkinter import messagebox
 
 
 class GUI(Frame):
     # constant
     NUMBER_COLLUM = 8
     NUMBER_ROW = 8
-    MYCOLLOR = None
     SIZE_SQUARE = 64
     WHITE = '#F0D9B5'
     BLACK = '#B58863'
@@ -21,10 +21,10 @@ class GUI(Frame):
 
 
 
-    def __init__(self, root, board, slot, mycollor):
+    def __init__(self, game, root, board, slot):
+        self.game = game
         self.board = board
         self.slot = slot
-        self.MYCOLLOR = mycollor
         super().__init__(root)
         #data
         self.piece = {}
@@ -95,8 +95,6 @@ class GUI(Frame):
         self.icon = ImageTk.PhotoImage(self.icon)
         self.piece['p'] = self.icon
 
-        #print(self.piece)
-
         #coodination of 64 square
         self.square = []
         offsetRank = (self.NUMBER_ROW - 1)* self.SIZE_SQUARE
@@ -104,8 +102,6 @@ class GUI(Frame):
             for j in range(self.NUMBER_COLLUM):
                 self.square.append([j*self.SIZE_SQUARE, offsetRank - i*self.SIZE_SQUARE])
 
-        for i in range(self.NUMBER_ROW*self.NUMBER_COLLUM):
-                print("%d %d" %(self.square[i][0], self.square[i][1]))
 
 
         BoardHeight = self.NUMBER_ROW * self.SIZE_SQUARE
@@ -120,50 +116,66 @@ class GUI(Frame):
         self.coordinateClick = []
         self.listHightLight = []
         self.mapHightLight = []
-
-        #control panel
-        self.controlPanel = Frame(self, width=300, background='yellow')
-        self.controlPanel.pack(side=tkinter.RIGHT, expand=True, fill=tkinter.Y)
-        self.buttonNewMatch = Button(self.controlPanel, text="new match",background="white")
-
-        self.buttonNewMatch.place(relx = .2, rely = .5, anchor = 'c')
-
-        #status bar
-        self.statusBar = tkinter.Frame(self, height=32)
-        self.statusBar.pack(side=tkinter.BOTTOM)
         self.draw()
+
+        #infomation
+        self.controlPanel = Frame(self, width= 300, background = "#d2f0bb")
+        self.controlPanel.pack(side=tkinter.LEFT, fill = tkinter.BOTH)
+
+        self.canvas = Canvas(self.controlPanel, background = 'white', height = 256)
+        self.canvas.create_text(20, 30, anchor='w', font="VNI-Dom 16",
+                           text="Information of game")
+        self.canvas.create_text(20, 60, anchor='w', font="VNI-Dom 14",
+                           text="Your color piece is white")
+        self.canvas.create_text(20, 90, anchor='w', font="VNI-Dom 14",
+                           text="Color piece of machine is black ")
+        self.canvas.create_text(20, 120, anchor='w', font="VNI-Dom 14",
+                           text="Alpha beta pruning depth = 3")
+        self.canvas.create_text(20, 150, anchor='w', font="VNI-Dom 14",
+                           text="Oponent move: ", tag = "move")
+        self.canvas.create_text(20, 220, anchor='w', font="VNI-Dom 14",
+                           text="Status board: your turn", tag = "status")
+        self.canvas.pack(fill=tkinter.BOTH, side = tkinter.TOP)
+
+        self.ButtonNewgame = tkinter.Button(self.controlPanel, text = "New game", width = 10, font="VNI-Dom 14", background = "#1698de")
+        self.ButtonNewgame.place(relx = .5, rely = .7, anchor ='c')
+        self.ButtonNewgame.bind('<Button-1>', self.clearBoard)
+
+    def clearBoard(self, event):
+        self.board.reset()
+        self.draw()
+        self.slot[0] = True
 
 
     def click(self, event):
-        if self.slot[0] == True or self.slot[0] == False:
-            x = event.x // self.SIZE_SQUARE
-            y = self.NUMBER_ROW - event.y // self.SIZE_SQUARE
-            self.coordinateClick.clear()
-            self.coordinateClick.append(x)
-            self.coordinateClick.append(y)
-            traceSquare = (self.NUMBER_ROW - 1 - event.y // self.SIZE_SQUARE)*self.NUMBER_ROW + (event.x // self.SIZE_SQUARE)
-            print(traceSquare)
-            if self.clickfrom is None:
-                pieceAt = self.board.piece_at(traceSquare)
-                if pieceAt is not None:
-                    self.clickfrom = self.YIELD[self.coordinateClick[0]] + str(self.coordinateClick[1])
-                    self.hightLight()
-                else:
-                    self.draw()
-            else:
-                if traceSquare in self.mapHightLight:
-                    self.move()
-                else:
-                    self.mapHightLight.clear()
-                    self.draw()
-                self.clickfrom = None
+        if self.slot[0] == True:
+            # check mate
 
+
+                x = event.x // self.SIZE_SQUARE
+                y = self.NUMBER_ROW - event.y // self.SIZE_SQUARE
+                self.coordinateClick.clear()
+                self.coordinateClick.append(x)
+                self.coordinateClick.append(y)
+                traceSquare = (self.NUMBER_ROW - 1 - event.y // self.SIZE_SQUARE)*self.NUMBER_ROW + (event.x // self.SIZE_SQUARE)
+                if self.clickfrom is None:
+                    pieceAt = self.board.piece_at(traceSquare)
+                    if pieceAt is not None:
+                        self.clickfrom = self.YIELD[self.coordinateClick[0]] + str(self.coordinateClick[1])
+                        self.hightLight()
+                    else:
+                        self.draw()
+                else:
+                    if traceSquare in self.mapHightLight:
+                        self.move()
+                    else:
+                        self.mapHightLight.clear()
+                        self.draw()
+                    self.clickfrom = None
 
     def hightLight(self):
         position = self.YIELD[self.coordinateClick[0]] + str(self.coordinateClick[1])
-        print (position)
         legal_moves =[str(legal_move) for legal_move in self.board.legal_moves]
-        print(legal_moves)
         self.listHightLight.clear()
         self.mapHightLight.clear()
         for legal_move in legal_moves:
@@ -172,27 +184,33 @@ class GUI(Frame):
         for legal_move in self.listHightLight:
             self.mapHightLight.append(self.MAP[legal_move[2]] + (int(legal_move[3]) - 1)*self.NUMBER_ROW)
 
-        print(self.mapHightLight)
         self.draw()
 
     def move(self):
         self.clickTo = self.YIELD[self.coordinateClick[0]] + str(self.coordinateClick[1])
         move = str(self.clickfrom) + str(self.clickTo)
+
         #check promotion
         traceFrom = self.MAP[self.clickfrom[0]] + (self.NUMBER_ROW) * (int(self.clickfrom[1]) - 1)
         traceFrom = self.board.piece_at(traceFrom)
-        print("trace" + str(traceFrom.symbol()))
-        print(self.clickTo[1])
-        print(traceFrom.symbol() == 'p' )
         if traceFrom.symbol() == 'p' and int(self.clickTo[1]) == 1:
             move += 'q'
         elif traceFrom.symbol() == 'P' and int(self.clickTo[1]) == 8:
             move += 'q'
-            print("hihi")
+
+        print(move)
         move = chess.Move.from_uci(move)
         self.board.push(move)
         self.mapHightLight.clear()
         self.draw()
+        self.slot[0] = False
+        self.canvas.delete("move")
+        self.canvas.create_text(20, 150, anchor='w', font="VNI-Dom 14",
+                           text="White move: " + str(move), tag = "move")
+        self.canvas.delete("status")
+        self.canvas.create_text(20, 220, anchor='w', font="VNI-Dom 14",
+                           text="Status board: Machine turn", tag = "status")
+        self.game.ROOT.after(1000, self.game.AIplay)
 
     def draw(self):
         #delete canvas
@@ -202,7 +220,7 @@ class GUI(Frame):
         positonClick = None
         if len(self.coordinateClick):
             positonClick = self.coordinateClick[0] + (self.NUMBER_ROW)* (self.coordinateClick[1] - 1)
-            print(positonClick)
+            #print(positonClick)
         for i in range(self.NUMBER_ROW):
             collor = self.BLACK if collor == self.WHITE else self.WHITE
             for j in range(self.NUMBER_COLLUM):
